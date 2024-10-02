fb40c019b4bcee9e4713581bac6230d9073ecb9e
yasinporsuk <yasinporsuk@gmail.com>
2024-10-02 13:08:47 +0200

    primer paso!

--- app/src/main/java/com/example/intentsapp/MainActivity.kt
+++ app/src/main/java/com/example/intentsapp/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             val context = this
             val intent = Intent(context, MainActivity2::class.java)
             intent.putExtra("nombre", nombre)
+
             this.startActivity(intent)
 
         }
