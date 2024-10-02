00ce30be58a2904ee64f32e2f8a7e8a079ca5737
yasinporsuk <yasinporsuk@gmail.com>
2024-10-02 13:40:28 +0200

    seegunod paso

--- app/src/main/java/com/example/intentsapp/MainActivity.kt
+++ app/src/main/java/com/example/intentsapp/MainActivity.kt
@@ -1,30 +1,42 @@
 package com.example.intentsapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.intentsapp.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.btnIntent.setOnClickListener{
-            var nombre = binding.etNombre.text.toString()
-            val context = this
-            val intent = Intent(context, MainActivity2::class.java)
-            intent.putExtra("nombre", nombre)
-
-            this.startActivity(intent)
-
+        binding.sendButton.setOnClickListener {
+            sendEmail()
         }
+    }
+
+    private fun sendEmail() {
+        val recipient = binding.recipientEditText.text.toString()
+        val message = binding.messageEditText.text.toString()
+        val subject = "Asunto"
+
+        // Crear el URI mailto
+        val emailUri = Uri.parse("mailto:${Uri.encode(recipient)}" +
+                "?subject=${Uri.encode(subject)}" +
+                "&body=${Uri.encode(message)}")
+
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = emailUri
+        }
+
+        startActivity(emailIntent)
     }
 }
